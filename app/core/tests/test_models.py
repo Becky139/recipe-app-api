91c3f506e0eb5a9c047e62b28b9a4110ac13f9c2
Becky139 <rebecca.marriott@outlook.com>
2023-12-18 10:01:59 +0000

    add email tests and run

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,7 +2,7 @@
 Tests for models
 """
 from django.test import TestCase
-from django.contrib.auth import ger_user_model
+from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
@@ -19,3 +19,16 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users."""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
